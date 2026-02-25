400fa8fd221ec0fb48c31a8a0d92a4a78dd3fd84
Harini Murugadas <murughar@students.zhaw.ch>
2026-02-25 11:17:10 +0100

    Email hinzugefügt

--- views/home.py
+++ views/home.py
@@ -6,8 +6,8 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Harini Murugadas (murughar@zhaw.ch)
-
+- Harini Murugadas (murughar@students.zhaw.ch)
+- Alexandra Vlk (vlkale01@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
