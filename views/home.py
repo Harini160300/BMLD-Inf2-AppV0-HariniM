f03952be99b80c699a6991a2ed56d37838e4343a
Harini Murugadas <murughar@students.zhaw.ch>
2026-03-25 11:29:21 +0100

    home geändert

--- views/home.py
+++ views/home.py
@@ -37,9 +37,4 @@ st.write("""
 Diese App wurde von folgenden Personen entwickelt:
 - Harini Murugadas (murughar@students.zhaw.ch)
 - Alexandra Vlk (vlkale01@students.zhaw.ch)
-
-Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
-
-Autor: Samuel Wehrli (wehs@zhaw.ch)
 """)
-
